6abea8a5e6ab8b80061b56f4a80bbe2fb3e069c5
Kevin Bui <0xc0dedead@gmail.com>
2025-02-07 01:07:11 +0900

    Fix SPM package

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     targets: [
         .target(
             name: "TokenML",
+            path: "TokenML",
             resources: [
                 .process("Generated/Tokens.mlmodel")
             ]
