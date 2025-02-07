e48f4ca90a805e1ec84435d6e0645417ca8af197
Kevin Bui <0xc0dedead@gmail.com>
2025-02-07 23:16:12 +0900

    Expose tokens model

--- TokenML/TokenML.swift
+++ TokenML/TokenML.swift
@@ -1,1 +1,15 @@
+import CoreML
+import Foundation
 
+public enum TokenML {
+    static let tokensModel: MLModel? = {
+        let configuration = MLModelConfiguration()
+        configuration.computeUnits = .all
+
+        guard let modelUrl = Bundle.module.url(forResource: "Tokens", withExtension: "mlmodelc") else {
+            return nil
+        }
+
+        return try? MLModel(contentsOf: modelUrl, configuration: configuration)
+    }()
+}
